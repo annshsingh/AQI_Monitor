dbfb6816e1fd31552bef3d282d77303a1e7a2793
Ashutosh Singh <ashutosh.singh@innovaccer.com>
2021-04-23 16:24:05 +0530

    setup fluro for naviation on web and mobile

---
+++ lib/routes/fluro_routes_handlers.dart
@@ -0,0 +1,34 @@
+import 'package:aqi_monitor/UI/home_page.dart';
+import 'package:aqi_monitor/UI/location_screen.dart';
+import 'package:aqi_monitor/model/city_model.dart';
+import 'package:fluro/fluro.dart';
+import 'package:flutter/material.dart';
+
+mixin FluroRoutesHandlers {
+  /// Handler for MyHomePage
+  var homeScreenHandler = Handler(
+    handlerFunc: (
+      BuildContext context,
+      Map<String, dynamic> parameters,
+    ) {
+      return MyHomePage();
+    },
+  );
+
+  /// Handler for LocationScreen
+  var locationScreenHandler = Handler(
+    handlerFunc: (
+      BuildContext context,
+      Map<String, dynamic> parameters,
+    ) {
+      CityModel cityModel;
+      if (parameters != null && parameters.isNotEmpty) {
+        cityModel = CityModel.fromQueryParam(parameters);
+      }
+      return LocationScreen(
+        city: cityModel != null ? cityModel.city : null,
+        // city: null,
+      );
+    },
+  );
+}
